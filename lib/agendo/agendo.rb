e809b448d1d318372cc057c72ba9fdd9cd4b32d2
estill01 <ehs.stillman@gmail.com>
2012-01-09 01:40:24 -0600

    first commit: agendo // top-level project structure

---
+++ lib/agendo/agendo.rb
@@ -0,0 +1,8 @@
+# this is where you include all the various other components...
+#
+# by structuring my system like this, I'm providing a way to let people
+# all-in-one include Agendo
+#
+# will likely go back and also break out the sub-systems and make them
+# individually accessible
+#
---
+++ lib/agendo/version.rb
@@ -0,0 +1,8 @@
+module Agendo
+	VERSION_NUMBERS = [
+		VERSION_MAJOR = 0,
+		VERSION_MINOR = 1,
+		VERSION_BUILD = 1
+	]
+	VERSION = VERSION_NUMBERS.join('.')
+end
